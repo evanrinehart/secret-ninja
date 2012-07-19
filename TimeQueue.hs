d3010e4b146968a14a64714e1c1dac7cba2b6aa4
Evan Rinehart <erinehart@searchinfluence.com>
2012-07-19 15:20:00 -0500

    refactor TimeQueue

--- TimeQueue.hs
+++ TimeQueue.hs
@@ -11,13 +11,15 @@ empty :: TimeQueue a
 empty = TQ []
 
 getReadyEvents :: GameTime -> TimeQueue a -> ([a], TimeQueue a)
-getReadyEvents t (TQ es) = (map snd l, TQ r) where
-  (l, r) = span ((<= t) . fst) es
+getReadyEvents t q = (map snd l, TQ r) where
+  (l,r) = splitAtTime t q
 
 schedule :: GameTime -> a -> TimeQueue a -> TimeQueue a
-schedule t x (TQ es) = TQ es' where
+schedule t x q = TQ es' where
   es' = l ++ (t,x) : r
-  (l,r) = span ((<= t) . fst) es
+  (l,r) = splitAtTime t q
+
+splitAtTime t (TQ es) = span ((<= t) . fst) es
 
 debug :: (a -> String) -> TimeQueue a -> String
 debug sh (TQ es) = unlines . map (\(t,x) -> show t ++ ": " ++ sh x) $ es
