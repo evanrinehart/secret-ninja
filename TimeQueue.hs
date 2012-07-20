6fd85f660606c09eec0fd37d2c02470356970dfe
Evan Rinehart <erinehart@searchinfluence.com>
2012-07-20 10:14:01 -0500

    moved uncurry (***) into operator

--- TimeQueue.hs
+++ TimeQueue.hs
@@ -12,7 +12,7 @@ empty :: TimeQueue a
 empty = TQ []
 
 getReadyEvents :: GameTime -> TimeQueue a -> ([a], TimeQueue a)
-getReadyEvents t q = (uncurry (***)) (map snd, TQ) (splitAtTime t q)
+getReadyEvents t q = (map snd, TQ) $$ (splitAtTime t q)
 
 schedule :: GameTime -> a -> TimeQueue a -> TimeQueue a
 schedule t x q = TQ es' where
@@ -20,6 +20,7 @@ schedule t x q = TQ es' where
   (l,r) = splitAtTime t q
 
 splitAtTime t (TQ es) = span ((<= t) . fst) es
+($$) = uncurry (***)
 
 debug :: (a -> String) -> TimeQueue a -> String
 debug sh (TQ es) = unlines . map (\(t,x) -> show t ++ ": " ++ sh x) $ es
