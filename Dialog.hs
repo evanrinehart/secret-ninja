4ca66fd3bb21e2311dab8e1de200a4c8dd62eb82
EvanR <evanrinehart@gmail.com>
2012-07-26 11:17:38 -0500

    added dialog monad

---
+++ Dialog.hs
@@ -0,0 +1,21 @@
+module Dialog where
+
+import Control.Monad
+
+data Dialog a = Answer a | Question String (String -> Dialog a)
+
+instance Monad Dialog where
+  return x = Answer x
+  (Answer x) >>= f = f x
+  (Question q cont) >>= f = Question q (cont >=> f)
+
+instance Functor Dialog where
+  fmap f d = d >>= return . f
+
+instance Show a => Show (Dialog a) where
+  show (Answer x) = "Answer " ++ show x
+  show (Question q _) = "Question " ++ show q
+
+question :: String -> Dialog String
+question q = Question q Answer
+
