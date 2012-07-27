4693b0c1b501fee0be540cb2721ad052b24bffca
EvanR <evanrinehart@gmail.com>
2012-07-27 00:39:28 -0500

    made dialog more polymorphic

--- Dialog.hs
+++ Dialog.hs
@@ -1,19 +1,21 @@
 module Dialog where
 
-data Dialog a = Answer a | Question String (String -> Dialog a)
+data Dialog t a = 
+  Answer a |
+  Question t (t -> Dialog t a)
 
-instance Monad Dialog where
+instance Monad (Dialog t) where
   return x = Answer x
   (Answer x) >>= f = f x
   (Question q cont) >>= f = Question q (\z -> cont z >>= f) -- (cont >=> f)
 
-instance Functor Dialog where
+instance Functor (Dialog t) where
   fmap f d = d >>= return . f
 
-instance Show a => Show (Dialog a) where
+instance (Show t, Show a) => Show (Dialog t a) where
   show (Answer x) = "Answer " ++ show x
   show (Question q _) = "Question " ++ show q
 
-question :: String -> Dialog String
+question :: t -> Dialog t t
 question q = Question q Answer
 
