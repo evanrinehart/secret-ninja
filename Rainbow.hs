90a96c945d8c1d97f34fbdeba386154e7e0f1d21
Evan Rinehart <erinehart@searchinfluence.com>
2012-08-05 17:16:45 -0500

    made Rainbow text module polymorphic

--- Rainbow.hs
+++ Rainbow.hs
@@ -24,29 +24,26 @@ data Color =
   BrightCyan |
   BrightWhite deriving (Eq,Ord,Show)
 
-data CFrag = NCF Text | CF Color Text deriving (Eq,Ord,Show)
-data Rainbow = Rainbow (Endo [CFrag])
+data CFrag a = NCF a | CF Color a deriving (Eq,Ord,Show)
+data Rainbow a = Rainbow (Endo [CFrag a])
 
-instance Monoid Rainbow where
+instance Monoid (Rainbow a) where
   Rainbow f `mappend` Rainbow g = Rainbow (f `mappend` g)
   mempty = Rainbow mempty
 
-instance IsString Rainbow where
-  fromString = fromText . fromString
+instance IsString a => IsString (Rainbow a) where
+  fromString = fragment . NCF . fromString
 
-instance Show Rainbow where
-  show ct = (show . encode) ct
+instance (IsString a, Show a, Monoid a) => Show (Rainbow a) where
+  show = show . compile
 
-fragment :: CFrag -> Rainbow
+fragment :: CFrag a -> Rainbow a
 fragment cf = (Rainbow . Endo) ([cf] ++)
 
-fromText :: Text -> Rainbow
-fromText = fragment . NCF
-
-color :: Color -> Text -> Rainbow
+color :: Color -> a -> Rainbow a
 color c txt = fragment (CF c txt)
 
-codeTab :: Color -> Text
+codeTab :: IsString a => Color -> a
 codeTab Black = "\ESC[30m"
 codeTab Red = "\ESC[31m"
 codeTab Green = "\ESC[32m"
@@ -64,12 +61,14 @@ codeTab BrightMagenta = "\ESC[1m\ESC[35m"
 codeTab BrightCyan = "\ESC[1m\ESC[36m"
 codeTab BrightWhite = "\ESC[1m\ESC[37m"
 
-reset :: Text
+reset :: IsString a => a
 reset = "\ESC[0m"
 
-encodeFragment :: CFrag -> [Text]
-encodeFragment (NCF txt) = [txt]
-encodeFragment (CF c txt) = [codeTab c, txt, reset]
+encodeFragment :: IsString a => CFrag a -> [a]
+encodeFragment (NCF a) = [a]
+encodeFragment (CF c a) = [codeTab c, a, reset]
 
-encode :: Rainbow -> Text
-encode (Rainbow (Endo f)) = (T.concat . concat . map encodeFragment) (f [])
+compile :: (IsString a, Monoid a) => Rainbow a -> a
+compile (Rainbow (Endo f)) = (mconcat . concat . map encodeFragment) (f [])
+
+
